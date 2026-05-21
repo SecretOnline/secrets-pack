b7048826dc2a24a5894618f08ce118d37f7aa831
secret_online <me@secretonline.co>
2026-05-21 18:52:20 +1200

    Update workflows for single project

--- .github/actions/get-build-matrix/index.js
+++ .github/actions/get-build-matrix/index.js
@@ -2,39 +2,10 @@ import { getInput, setOutput } from "@actions/core";
 import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 
-/** @type {PackSettingsDefinition[]} */
-const ALL_PACKS = [
-  {
-    type: "modpack",
-    pack: "secrets-pack",
-    modpackTests: [
-      { client: "false", server: "modded" },
-      { client: "modded", server: "vanilla" },
-    ],
-  },
-  {
-    type: "modpack",
-    pack: "eths-pack",
-    // Forge is being too slow so we're just not testing it :(
-    // modpackTests: [{ client: "modded", server: "modded" }],
-  },
-  {
-    type: "modpack",
-    pack: "create-little-bit",
-    modpackTests: [{ client: "modded", server: "modded" }],
-  },
-  {
-    type: "resourcepack",
-    pack: "enor-pearl",
-  },
-  {
-    type: "resourcepack",
-    pack: "treehouse-paintings",
-  },
-  {
-    type: "resourcepack",
-    pack: "well-known-art",
-  },
+/** @type {ModpackTestEntry[]} */
+const MODPACK_TESTS = [
+  { client: "false", server: "modded" },
+  { client: "modded", server: "vanilla" },
 ];
 
 const changedFiles = getInput("changed-files", {
@@ -44,77 +15,53 @@ const changedFiles = getInput("changed-files", {
   .filter(Boolean);
 
 const containsRelevantFiles = changedFiles.some((filePath) =>
-  filePath.match(/^(?:modpack|resourcepack|datapack)s[\//]/),
+  filePath.match(/^\d[\d.]+\//),
 );
 if (!containsRelevantFiles) {
   console.log("No files related to packs were changed, building all packs");
 }
 
-/**
- * @param {PackSettingsDefinition} pack
- * @returns {Promise<ResolvedPack[]>}
- */
-async function expandPackDefinition(pack) {
-  /** @type {{ name: string; 'modrinth-id': string; 'version-map': { [key: string]: { directory: string; 'game-versions': string, 'should-run-workflows': boolean } } }} */
-  const project = JSON.parse(
-    await readFile(
-      join(process.cwd(), `${pack.type}s`, pack.pack, "project.json"),
-      {
-        encoding: "utf-8",
-      },
-    ),
-  );
+/** @type {{ name: string; 'modrinth-id': string; 'version-map': { [key: string]: { directory: string; 'game-versions': string; 'should-run-workflows': boolean } } }} */
+const project = JSON.parse(
+  await readFile(join(process.cwd(), "project.json"), { encoding: "utf-8" }),
+);
 
-  return Object.values(project["version-map"]).map((version) => ({
-    ...pack,
-    directory: version.directory,
-    shouldRunWorkflows: version["should-run-workflows"],
-    name: project.name,
-  }));
-}
+/** @type {VersionEntry[]} */
+const allVersions = Object.values(project["version-map"]).map((version) => ({
+  directory: version.directory,
+  shouldRunWorkflows: version["should-run-workflows"],
+}));
 
 /**
- * @param {ResolvedPack} pack
+ * @param {VersionEntry} version
  * @returns {boolean}
  */
-function filterByChangedFiles(pack) {
+function filterByChangedFiles(version) {
   if (!containsRelevantFiles) {
     return true;
   }
-
-  const regex = new RegExp(`^${pack.type}s/${pack.pack}/${pack.directory}/`);
+  const regex = new RegExp(`^${version.directory}/`);
   return changedFiles.some((filePath) => filePath.match(regex));
 }
 
-const resolvedPacks = (
-  await Promise.all(ALL_PACKS.map(expandPackDefinition))
-).flatMap((arr) => arr);
-const changedPacks = resolvedPacks.filter(filterByChangedFiles);
+const changedVersions = allVersions.filter(filterByChangedFiles);
 console.log(
-  `Found ${changedPacks.length} packs to build: ${changedPacks.map((pack) => pack.name).join(", ")}`,
+  `Found ${changedVersions.length} versions to build: ${changedVersions.map((v) => v.directory).join(", ")}`,
 );
 
 /** @type {BuildMatrixEntry[]} */
-const builds = changedPacks
-  .filter((pack) => pack.shouldRunWorkflows)
-  .map((pack) => {
-    const note = pack.directory === "pack" ? "" : pack.directory;
-    const title =
-      `Build ${pack.type} ${pack.name} ${note ? `(${note})` : ""}`.trim();
+const builds = changedVersions
+  .filter((v) => v.shouldRunWorkflows)
+  .map((v) => ({
+    title: `Build ${project.name} (${v.directory})`,
+    directory: v.directory,
+  }));
 
-    return {
-      title,
-      type: pack.type,
-      pack: pack.pack,
-      directory: pack.directory,
-    };
-  });
-
-/** @type {BuildMatrixEntry[]} */
-const modpackTests = changedPacks
-  .filter((pack) => pack.type === "modpack" && pack.shouldRunWorkflows)
-  .flatMap((pack) =>
-    (pack.modpackTests ?? []).map((test) => {
+/** @type {TestMatrixEntry[]} */
+const modpackTests = changedVersions
+  .filter((v) => v.shouldRunWorkflows)
+  .flatMap((v) =>
+    MODPACK_TESTS.map((test) => {
       /** @type {string[]} */
       const notes = [];
       switch (test.client) {
@@ -140,34 +87,24 @@ const modpackTests = changedPacks
       }
       const note = notes.join(", ");
       const title =
-        `Test ${pack.name} ${pack.directory} ${note ? `(${note})` : ""}`.trim();
+        `Test ${project.name} ${v.directory} ${note ? `(${note})` : ""}`.trim();
 
       return {
         title,
-        type: pack.type,
-        pack: pack.pack,
-        directory: pack.directory,
+        directory: v.directory,
         client: test.client,
         server: test.server,
       };
     }),
   );
 
-/** @type {BuildMatrixEntry[]} */
-const modpackUpdates = changedPacks
-  .filter((pack) => pack.type === "modpack" && pack.shouldRunWorkflows)
-  .map((pack) => {
-    const note = pack.directory === "pack" ? "" : pack.directory;
-    const title =
-      `Build ${pack.type} ${pack.name} ${note ? `(${note})` : ""}`.trim();
-
-    return {
-      title,
-      type: pack.type,
-      pack: pack.pack,
-      directory: pack.directory,
-    };
-  });
+/** @type {UpdateMatrixEntry[]} */
+const modpackUpdates = changedVersions
+  .filter((v) => v.shouldRunWorkflows)
+  .map((v) => ({
+    title: `Update ${project.name} (${v.directory})`,
+    directory: v.directory,
+  }));
 
 setOutput("has-builds", (builds.length > 0).toString());
 setOutput("build-matrix", JSON.stringify(builds));
@@ -179,8 +116,9 @@ setOutput("update-modpack-mods-matrix", JSON.stringify(modpackUpdates));
 // Types are down here so they don't take up too much space at the top
 
 /**
- * @typedef PackType
- * @type {'modpack' | 'resourcepack' | 'datapack'}
+ * @typedef VersionEntry
+ * @property {string} directory
+ * @property {boolean} shouldRunWorkflows
  */
 
 /**
@@ -195,36 +133,21 @@ setOutput("update-modpack-mods-matrix", JSON.stringify(modpackUpdates));
  */
 
 /**
- * @typedef PackSettingsDefinition
- * @property {PackType} type
- * @property {string} pack
- * @property {ModpackTestEntry[]} [modpackTests]
- */
-
-/**
- * @typedef ResolvedPack
- * @property {string} name
- * @property {PackType} type
- * @property {string} pack
- * @property {string} directory
- * @property {boolean} shouldRunWorkflows
- * @property {ModpackTestEntry[]} [modpackTests]
- */
-
-/**
  * @typedef BuildMatrixEntry
  * @property {string} title
- * @property {PackType} type
- * @property {string} pack
  * @property {string} directory
  */
 
 /**
- * @typedef TestModpackMatrixEntry
+ * @typedef TestMatrixEntry
  * @property {string} title
- * @property {PackType} type
- * @property {string} pack
  * @property {string} directory
  * @property {ModpackTestEntryMode} client
  * @property {ModpackTestEntryMode} server
+ */
+
+/**
+ * @typedef UpdateMatrixEntry
+ * @property {string} title
+ * @property {string} directory
  */
--- .github/actions/get-deploy-info/index.js
+++ .github/actions/get-deploy-info/index.js
@@ -2,18 +2,12 @@ import { getInput, setOutput } from "@actions/core";
 import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 
-const typeAndPack = getInput("pack", { required: true });
 const versionNumber = getInput("version", { required: true });
 
-const PACK_INPUT_REGEX = /^([^/]+)\/([^/]+)$/;
+const type = "modpack";
+const pack = "secrets-pack";
+
 const VERSION_REGEX = /^(\d+)\.(\d+)\.(\d+)$/;
-
-const typeAndPackMatch = PACK_INPUT_REGEX.exec(typeAndPack);
-if (!typeAndPackMatch) {
-  throw new Error(`Pack input (${typeAndPack}) is not valid`);
-}
-
-const [, type, pack] = typeAndPackMatch;
 
 const versionMatch = VERSION_REGEX.exec(versionNumber);
 if (!versionMatch) {
@@ -23,7 +17,7 @@ if (!versionMatch) {
 const [, versionMajor] = versionMatch;
 /** @type {{ name: string; 'modrinth-id': string; 'version-map': { [key: string]: { directory: string; 'game-versions': string, 'should-run-workflows': boolean } } }} */
 const project = JSON.parse(
-  await readFile(join(process.cwd(), `${type}s`, pack, "project.json"), {
+  await readFile(join(process.cwd(), "project.json"), {
     encoding: "utf-8",
   }),
 );
@@ -36,44 +30,23 @@ const versionDirName = project["version-map"][projectEntry].directory;
 const gameVersions = project["version-map"][projectEntry]["game-versions"];
 const releaseName = `${project.name} v${versionNumber} for Minecraft ${gameVersions}`;
 
-// TODO: Get loader properly
-/** @type {string} */
-let loaders;
-if (type === "modpack") {
-  const packToml = await readFile(
-    join(process.cwd(), `${type}s`, pack, versionDirName, "pack.toml"),
-    "utf-8",
-  );
+const packToml = await readFile(
+  join(process.cwd(), versionDirName, "pack.toml"),
+  "utf-8",
+);
 
-  const loaderKeys = ["fabric", "quilt", "forge", "neoforge"];
-  const loaderRegex = new RegExp(
-    `\\[versions](?:.|[\\r\\n])+^(${loaderKeys.join("|")}) = .*`,
-    "gm",
-  );
+const loaderKeys = ["fabric", "quilt", "forge", "neoforge"];
+const loaderRegex = new RegExp(
+  `\\[versions](?:.|[\\r\\n])+^(${loaderKeys.join("|")}) = .*`,
+  "gm",
+);
 
-  const loaderMatch = loaderRegex.exec(packToml);
-  if (!loaderMatch) {
-    throw new Error(`Could not find loaders in pack.toml`);
-  }
-
-  loaders = loaderMatch[1];
-} else {
-  loaders = "minecraft";
+const loaderMatch = loaderRegex.exec(packToml);
+if (!loaderMatch) {
+  throw new Error(`Could not find loaders in pack.toml`);
 }
 
-/** @type {string[]} */
-const files = [];
-let includeSource = false;
-if (type === "modpack") {
-  files.push(`${pack}.${versionNumber}.mrpack`);
-
-  if (pack === "secrets-pack") {
-    files.push(`source-code.${pack}.${versionNumber}.zip.mrpack`);
-    includeSource = true;
-  }
-} else {
-  files.push(`${pack}.${versionNumber}.zip`);
-}
+const loaders = loaderMatch[1];
 
 let modrinthId = project["modrinth-id"] ?? "";
 if (modrinthId === "null") {
@@ -94,8 +67,6 @@ if (modrinthId) {
 }
 
 setOutput("artifact-name", `${pack}.${versionDirName}`);
-setOutput("type", type);
-setOutput("pack", pack);
 setOutput("directory", versionDirName);
 setOutput("modrinth-id", modrinthId);
 setOutput("pack-name", project.name);
@@ -103,5 +74,3 @@ setOutput("release-name", releaseName);
 setOutput("version-name", `v${versionNumber}`);
 setOutput("game-versions", gameVersions);
 setOutput("loaders", loaders);
-setOutput("files", files.join("\n"));
-setOutput("include-source", includeSource.toString());
